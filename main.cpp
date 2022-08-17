7795b03d4d2d9890db20fb789459a38c4748fdcf
svip K <1145252643@qq.com>
2022-08-17 12:09:03 +0800

    释放版本

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include "readwritefile.h"
 int main(int argc, char *argv[])
 {
-    if(!ReadWriteFile::isexist("language.txt")){
-        ReadWriteFile::WriteCach("en","language.txt");
+    if(!ReadWriteFile::isexist("./language.txt")){
+        ReadWriteFile::WriteCach("en","./language.txt");
     }
 
     //检查语言设置
-    if(ReadWriteFile::ReadCach(1,"language.txt").at(0).toStdString()=="zh"){
+    if(ReadWriteFile::ReadCach(1,"./language.txt").at(0).toStdString()=="zh"){
         QApplication a(argc, argv);
         QTranslator translator;
         if(translator.load("zh.qm")){
